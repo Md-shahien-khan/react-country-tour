eba582bb7d4ff34a0852676a8a579da746a012aa
Md shahien khan <mdshahienkhan@Mds-MacBook-Air.local>
2024-11-26 13:18:10 +0000

    Added function to component and send it to another component

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -3,18 +3,40 @@ import Country from "./Country";
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCounteries, setVisitedCounteries] = useState([]);
+
+
     useEffect(() =>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setCountries(data));
-    }, [])
+    }, []);
+
+
+    const handleVisitedCountry = countryItem =>{
+        console.log('Add this to your visited countries');
+        const newVisitedCountries = [...visitedCounteries, countryItem];
+        setVisitedCounteries(newVisitedCountries);
+    }
     // If there is some object inside an array then initial value will be empty array in the state.
     return (
-        <div>
+        <div className="border-2 p-4">
             <h3 className="font-bold">Countries Length : {countries.length}</h3>
+            <div>
+                <h5>Visited Countries {visitedCounteries.length}</h5>
+                <ul>
+                    {
+                        visitedCounteries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                countries.map(country => <Country 
+                    key={country.cca3} 
+                    country={country}
+                    handleVisitedCountry={handleVisitedCountry}
+                    ></Country>)
             }  
             </div>
         </div>
--- src/Components/Countries/Country.jsx
+++ src/Components/Countries/Country.jsx
@@ -1,13 +1,32 @@
-const Country = ({country}) => {
+import { useState } from "react";
+
+const Country = ({country, handleVisitedCountry}) => {
+
     const {name, flags, population, region, cca2, area} = country;
+    const [visited, setVisited] = useState(false);
+    
+    const handleVisited = () =>{
+        setVisited(!visited);
+    };
+
+    // console.log(handleVisitedCountry);
+
+
     return (
-        <div className="p-2 border-2 w-7/12 mx-auto m-2 rounded-lg bg-slate-100">
-            <h3 className="font-bold">Name : {name.common}</h3>
+        <div className="border-2">
+            <div className="p-2 border-2 w-7/12 mx-auto m-2 rounded-lg bg-slate-100">
+            <h3 className={`font-bold ${visited ? 'text-pink-500' : 'text-black'}`}>Name : {name.common}</h3>
             <img className="w-full h-[100px]" src={flags.png} alt="" />
             <p>Population : {population}</p>
             <p>Region : {region}</p>
             <p>{cca2}</p>
             <p>Area : {area}</p>
+            <button className="btn bg-slate-500 rounded-md p-2" onClick={handleVisited}>{visited ? 'Visited' : 'Will Visit'}</button> <br />
+            {/* {visited && 'I have visited this country.'} */}
+            {visited ? 'I have visited this country.' : 'I want to visit this country'};
+
+            <button onClick={() => handleVisitedCountry(country)} className="btn bg-red-300 p-2 rounded-lg">Mark Visited</button>
+        </div>
         </div>
     );
 };
