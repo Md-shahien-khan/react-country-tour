286ce11fe31a62dbb0ddd81d3da22643cc9a798a
Md shahien khan <mdshahienkhan@Mds-MacBook-Air.local>
2024-11-26 15:10:39 +0000

    project done

---
+++ src/Components/Countries/CountryDetail.jsx
@@ -0,0 +1,12 @@
+// Context Api
+// const CountryDetail = (props) => {
+//     // const {country, handleVisitedCountry, handleVisitedFlags} = props;
+//     return (
+//         <div>
+//             <CountryAgain {...props}></CountryAgain>
+                // you can send all the prop like ... way
+//         </div>
+//     );
+// };
+
+// export default CountryDetail;
